079d017501b40628903726efbe0cfc3dee91a61f
Yaw Fosu <codehumps00233@gmail.com>
2026-09-11 04:18:18 -0700

    innitial setup

--- vendorhub/README.md
+++ vendorhub/README.md
@@ -1,2 +1,3 @@
 # VendorHub-Frontend-B1
+
 The Frontend Repository for the VendorHub First Batch
---
+++ vendorhub/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full  h-screen flex justify-center items-center">
+      <h1 className="text-6xl text-black font-extrabold">
+        Welcome to Vendor
+        <span className="text-orange-500 animate-ping">Hub</span>
+      </h1>
+    </div>
+  );
+}
