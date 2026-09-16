e2ab04cf8fd51b890591f944ab3220fcf4702367
Yaw Fosu <codehumps00233@gmail.com>
2026-09-16 05:02:53 -0700

    refactor(auth): restructure vendor registration flow and update form components

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import { VendorRegistrationFlow } from "@/components/registration/VendorRegistrationFlow";
+import RegistrationFlow from "@/components/registration/RegistrationFlow";
+
 import OnboardingGraphics from "@/components/shared/OnboardingGraphics";
 
 export default function RegisterPage() {
   return (
-    <div className="w-full min-h-screen md:h-screen md:p-4 bg-white flex justify-center items-center">
+    <div className="w-full min-h-screen md:h-screen md:p-4 bg-orange-100 flex justify-center items-center">
       <div className="flex w-full md:max-w-7xl h-full md:h-[90vh]  overflow-hidden md:m-auto">
         {/* LEFT SIDE: Onboarding Graphics (Hidden on Mobile) */}
         <OnboardingGraphics />
 
         {/* RIGHT SIDE: Form Layout Shell */}
-        <VendorRegistrationFlow />
+        <RegistrationFlow />
       </div>
     </div>
   );
