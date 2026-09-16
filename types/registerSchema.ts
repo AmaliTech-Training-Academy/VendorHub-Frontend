0f23df568da089ea6210b22f91cdbec7c868b5eb
Yaw Fosu <codehumps00233@gmail.com>
2026-09-16 03:24:03 -0700

    fix(auth): fix validation for businessName and ownerName fields value

--- types/registerSchema.ts
+++ types/registerSchema.ts
@@ -1,8 +1,15 @@
 import { z } from "zod";
 
 export const registerSchema = z.object({
-  businessName: z.string("Please this field required only string"),
-  ownerName: z.string("Please this field required only strings"),
+  businessName: z
+    .string()
+    .min(3, "Business name must be at least 3 characters")
+    .max(50, "Business name must be under 50 characters"),
+
+  ownerName: z
+    .string()
+    .min(3, "Owner name must be at least 3 characters")
+    .max(50, "Owner name must be under 50 characters"),
   email: z.email("Please enter a valid email"),
   password: z.string().min(8, "Password must be at least 8 characters"),
 });
